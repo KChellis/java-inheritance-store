d0226d3d7e9969f543a1edb2dbfc696198008414
Matt Dowdney <matt@>
2018-06-12 14:42:16 -0700

    add tests for dogItems

--- src/main/java/dao/Sql2oDogItemsDao.java
+++ src/main/java/dao/Sql2oDogItemsDao.java
@@ -1,4 +1,75 @@
 package dao;
 
-public class Sql2oDogItemsDao {
+import models.DogItems;
+import models.Item;
+import models.SmallAnimalItems;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sql2oDogItemsDao implements DogItemsDao{
+    private final Sql2o sql2o;
+    public Sql2oDogItemsDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public void add(DogItems dogItems) {
+        String sql = "INSERT INTO items (type, priceInCents, discountAsPercentage, description) VALUES (:type, :priceInCents, :discountAsPercentage, :description)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(dogItems)
+                    .executeUpdate()
+                    .getKey();
+            dogItems.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public List<DogItems> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM items WHERE type = 'dog'")
+                    .executeAndFetch(DogItems.class);
+        }
+
+    }
+
+
+    @Override
+    public DogItems findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM items WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(DogItems.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from items WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from items WHERE type ='dog'";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
 }
--- src/test/java/dao/Sql2oCatItemsDaoTest.java
+++ src/test/java/dao/Sql2oCatItemsDaoTest.java
@@ -1,38 +1,81 @@
 package dao;
 
+import models.SmallAnimalItems;
+import models.CatItems;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oCatItemsDaoTest {
+    private Sql2oSmallAnimalItemsDao smallAnimalItemsDao;
+    private Sql2oCatItemsDao catItemsDao;
+    private Connection conn;
 
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        smallAnimalItemsDao = new Sql2oSmallAnimalItemsDao(sql2o);
+        catItemsDao = new Sql2oCatItemsDao(sql2o);
+        conn = sql2o.open();
+    }
+    public CatItems setupCatItems(){
+        return new CatItems(599, 0, "salmon jerky");
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
 
     @Test
     public void add() {
+        CatItems catItems = setupCatItems();
+        int idofOriginal = catItems.getId();
+        catItemsDao.add(catItems);
+        assertNotEquals(idofOriginal, catItems.getId());
     }
 
     @Test
     public void getAll() {
+        CatItems catItems = setupCatItems();
+        CatItems catItemsTwo = setupCatItems();
+        catItemsDao.add(catItems);
+        catItemsDao.add(catItemsTwo);
+        assertEquals(2, catItemsDao.getAll().size());
     }
 
     @Test
     public void findById() {
+        CatItems catItems = setupCatItems();
+        catItemsDao.add(catItems);
+        CatItems foundCatItems = catItemsDao.findById(catItems.getId());
+        assertEquals(catItems, foundCatItems);
     }
 
     @Test
     public void deleteById() {
+        CatItems catItems = setupCatItems();
+        CatItems catItemsTwo = setupCatItems();
+        catItemsDao.add(catItems);
+        catItemsDao.add(catItemsTwo);
+        catItemsDao.deleteById(1);
+        assertEquals(1, catItemsDao.getAll().size());
     }
 
     @Test
     public void clearAll() {
+        CatItems catItems = setupCatItems();
+        CatItems catItemsTwo = setupCatItems();
+        SmallAnimalItems smallAnimalItems = new SmallAnimalItems(399, 15, "fish tank gravel");
+        catItemsDao.add(catItems);
+        catItemsDao.add(catItemsTwo);
+        smallAnimalItemsDao.add(smallAnimalItems);
+        catItemsDao.clearAll();
+        assertEquals(1, smallAnimalItemsDao.getAllFromAllTypes().size());
     }
 }
